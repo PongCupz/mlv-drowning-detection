c3cfb3e270868efb419db318684914f22a0e6e1c
pongsak.l <pongsak.l@bluepi.co.th>
2023-03-25 20:40:24 +0700

    update params

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from utility.restnest50 import RestNet50
 
-rest_net = RestNet50.RestNet50("data/datasets/mlv/")
-rest_net.test('data/datasets/mlv/validation/drowning/*')
+rest_net = RestNet50.RestNet50()
+rest_net.test('data/datasets/mlv/validation/drowning/*',"weights-drowning2")
--- utility/restnest50/RestNet50.py
+++ utility/restnest50/RestNet50.py
@@ -12,9 +12,7 @@ import torch.optim as optim
 import torchvision
 
 class RestNet50:
-    def __init__(self,path):
-        self.input_path = path
-
+    def __init__(self):
         self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                         std=[0.229, 0.224, 0.225])
 
@@ -36,39 +34,27 @@ class RestNet50:
         }
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    def train(self):
+    def train(self,input_path,model_name,batch_size=32,epochs=3):
         self.image_datasets = {
             'train': 
-            datasets.ImageFolder(self.input_path  + 'train', self.data_transforms['train']),
+            datasets.ImageFolder(input_path  + 'train', self.data_transforms['train']),
             'validation': 
-            datasets.ImageFolder(self.input_path  + 'validation', self.data_transforms['validation'])
+            datasets.ImageFolder(input_path  + 'validation', self.data_transforms['validation'])
         }
 
         self.dataloaders = {
             'train':
             torch.utils.data.DataLoader(self.image_datasets['train'],
-                                        batch_size=32,
+                                        batch_size=batch_size,
                                         shuffle=True,
                                         num_workers=0),  # for Kaggle
             'validation':
             torch.utils.data.DataLoader(self.image_datasets['validation'],
-                                        batch_size=32,
+                                        batch_size=batch_size,
                                         shuffle=False,
                                         num_workers=0)  # for Kaggle
         }
 
-        self.dataloaders = {
-            'train':
-            torch.utils.data.DataLoader(self.image_datasets['train'],
-                                        batch_size=32,
-                                        shuffle=True,
-                                        num_workers=0),  # for Kaggle
-            'validation':
-            torch.utils.data.DataLoader(self.image_datasets['validation'],
-                                        batch_size=32,
-                                        shuffle=False,
-                                        num_workers=0)  # for Kaggle
-        }
 
         model = models.resnet50(weights='ResNet50_Weights.DEFAULT').to(self.device)
             
@@ -84,17 +70,17 @@ class RestNet50:
         criterion = nn.CrossEntropyLoss()
         optimizer = optim.Adam(model.fc.parameters())
 
-        model_trained = self.train_model(model, criterion, optimizer, num_epochs=3)
-        torch.save(model_trained.state_dict(), 'models/weights-drowning.h5')
+        model_trained = self.train_model(model, criterion, optimizer, num_epochs=epochs)
+        torch.save(model_trained.state_dict(), f"models/{model_name}.h5")
 
-    def test(self,test_set):
+    def test(self,test_set,model_name):
 
         model = models.resnet50().to(self.device)
         model.fc = nn.Sequential(
                     nn.Linear(2048, 128),
                     nn.ReLU(inplace=True),
                     nn.Linear(128, 2)).to(self.device)
-        model.load_state_dict(torch.load('models/weights-drowning.h5'))
+        model.load_state_dict(torch.load(f"models/{model_name}.h5"))
 
         d_files = glob.glob(test_set) 
         img_list = [Image.open(img_path) for img_path in d_files]
